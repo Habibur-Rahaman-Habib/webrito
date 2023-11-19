88cc38dbe336b750eb30df743811d73f375ec5b7
Habibur Rahaman <habib5501659@gmail.com>
2023-11-19 09:45:04 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,40 @@
-function($){
-//write here the jquery code
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}}(jQuery);
+(function ($) {
+
+
+  $(".service_list_slider").slick({
+    slidesToShow: 3,
+    arrows: false,
+    dots: false,
+    autoplay: false,
+    speed: 10000,
+    autoplaySpeed: 100,
+    cssEase: 'linear',
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 765,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+
+
+
+
+
+
+})(jQuery);
