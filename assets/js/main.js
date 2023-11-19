51099b60686c9e6f488766b4b9e1dc35905535ca
Habibur Rahaman <habib5501659@gmail.com>
2023-11-19 17:30:26 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,11 @@
 (function ($) {
 
 
-  $(".service_list_slider").slick({
-    slidesToShow: 3,
-    arrows: false,
-    dots: false,
+  $(".client_project_slider").slick({
     autoplay: true,
-    speed: 10000,
-    autoplaySpeed: 100,
-    cssEase: 'linear',
-    pauseOnHover: true,
+    slidesToShow: 2,
+    prevArrow: '<button class="prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow: '<button class="next-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
     responsive: [
       {
         breakpoint: 1150,
@@ -20,21 +16,18 @@
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 2,
-        }
-      },
-      {
-        breakpoint: 765,
-        settings: {
           slidesToShow: 1,
         }
       },
     ]
   });
 
-
-
-
-
+  // have a project counter
+  $(document).ready(function () {
+    $('.count').counterUp({
+      delay: 10,
+      time: 5000
+    });
+  });
 
 })(jQuery);
