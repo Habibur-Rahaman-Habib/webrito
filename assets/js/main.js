6d54674fdd7b529665bd997837d20b444a05848f
Habibur Rahaman <habib5501659@gmail.com>
2023-11-20 17:30:49 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,60 @@
     ]
   });
 
+  // testimonial
+  $('.testi_ltr_slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    cssEase: 'linear',
+    // centerMode: true,
+    draggable: true,
+    pauseOnFocus: true,
+
+    // infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 0,
+    // speed: 8000,
+    // slidesToShow: 4,
+    // arrows: false,
+    // dots: false,
+    // centerMode: true,
+    // cssEase: 'linear',
+    // pauseOnFocus: true,
+  });
+
+  $('.testi_rtl_slider').slick({
+
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 10000,
+    cssEase: 'linear',
+    centerMode: true,
+    rtl: true,
+
+    // infinite: true,
+    // autoplay: true,
+    // autoplaySpeed: 0,
+    // speed: 8000,
+    // slidesToShow: 4,
+    // arrows: false,
+    // dots: false,
+    // centerMode: true,
+    // // cssEase: 'linear',
+    // // pauseOnFocus: true,
+    // rtl: true,
+  });
+
   // have a project counter
   $(document).ready(function () {
     $('.count').counterUp({
@@ -30,4 +84,56 @@
     });
   });
 
+
 })(jQuery);
+
+//read more
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = 'Read more <i class="fa-solid fa-angle-down"></i>';
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = 'Read less <i class="fa-solid fa-angle-up"></i>';
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction2() {
+  var dots = document.getElementById("dotss");
+  var moreText = document.getElementById("moree");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = 'Read more <i class="fa-solid fa-angle-down"></i>';
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = 'Read less <i class="fa-solid fa-angle-up"></i>';
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction3() {
+  var dots = document.getElementById("dotsss");
+  var moreText = document.getElementById("moreeee");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = 'Read more <i class="fa-solid fa-angle-down"></i>';
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = 'Read less <i class="fa-solid fa-angle-up"></i>';
+    moreText.style.display = "inline";
+  }
+
+}
+
